37eb808fde414e65f4687057dab821c92241cfef
Victor Perron <victor@iso3103.net>
2018-12-23 10:58:40 +0100

    event-card: Add first test

---
+++ src/components/event-card.spec.js
@@ -0,0 +1,49 @@
+import moment from 'moment'
+
+import { shallowMount, trimHtml } from 'src/testing'
+
+import EventCard from './event-card.vue'
+
+const myEvent = {
+  name: 'Big GN',
+  start: moment('2020-05-12T01:00:00Z'),
+  summary: 'some bigass larp',
+  distance: 200,
+  readable_cost: '50 CHF',
+  raw: {
+    location: {
+      name: '212 rue de Tolbiac',
+    },
+  },
+}
+
+describe('EventCard', () => {
+  it('should display', () => {
+    const wrapper = shallowMount(EventCard, {
+      propsData: {
+        event: myEvent,
+      },
+    })
+    expect(wrapper.html()).toContain(trimHtml(`
+    <div class="event-card" style="color: rgb(153, 153, 153);">
+      <div class="name">Big GN</div>
+      <heart-stub event="[object Object]"></heart-stub>
+      <div class="date-details">
+        <span>May 12, 2020</span>
+        <span>&nbsp;|&nbsp;</span>
+        <span></span>
+      </div>
+      <div class="separator"></div>
+      <div class="description">some bigass larp</div>
+      <div class="location-details">
+        <div class="location">
+          <span>212 rue de Tolbiac</span>
+          <span>&nbsp;|&nbsp;</span>
+          <span>200kms</span>
+        </div>
+        <div class="price">50 CHF</div>
+      </div>
+    </div>
+    `))
+  })
+})
